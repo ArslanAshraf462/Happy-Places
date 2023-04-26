ed6315956fc774e031047a6ed609ac8d43a85cec
arslanashraf006 <arslan.ashraf.oraxtech@gmail.com>
2023-04-26 18:40:44 +0500

    Added -> using a third party library for nice rounded images

--- app/src/main/java/com/example/happyplaces/activities/MainActivity.kt
+++ app/src/main/java/com/example/happyplaces/activities/MainActivity.kt
@@ -1,15 +1,23 @@
 package com.example.happyplaces.activities
 
 import android.content.Intent
+import android.opengl.Visibility
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.happyplaces.R
+import com.example.happyplaces.adapters.HappyPlacesAdapter
 import com.example.happyplaces.database.DatabaseHandler
 import com.example.happyplaces.models.HappyPlaceModel
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
+    private var rvHappyPlaceList : RecyclerView? = null
+    private var tvNoRecordAvailable : TextView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -21,14 +29,27 @@ class MainActivity : AppCompatActivity() {
         }
         getHappyPlacesListFromLocalDB()
     }
+    private fun setupHappyPlacesRecycleView(happyPlaceList: ArrayList<HappyPlaceModel>){
+         rvHappyPlaceList = findViewById(R.id.rv_happy_places_list)
+        rvHappyPlaceList?.layoutManager = LinearLayoutManager(this)
+
+        rvHappyPlaceList?.setHasFixedSize(true)
+        val placesAdapter = HappyPlacesAdapter(this,happyPlaceList)
+        rvHappyPlaceList?.adapter = placesAdapter
+    }
     private fun getHappyPlacesListFromLocalDB(){
+        tvNoRecordAvailable = findViewById(R.id.tv_no_record_available)
         val dbHandler = DatabaseHandler(this)
         val getHappyPlaceList : ArrayList<HappyPlaceModel> = dbHandler.getHappyPlacesList()
         if(getHappyPlaceList.size >0){
             for (i in getHappyPlaceList){
-                Log.e("Title", i.title)
-                Log.e("Description", i.description)
+                rvHappyPlaceList?.visibility = View.VISIBLE
+                tvNoRecordAvailable?.visibility = View.GONE
+                setupHappyPlacesRecycleView(getHappyPlaceList)
             }
+        }else{
+            rvHappyPlaceList?.visibility = View.GONE
+            tvNoRecordAvailable?.visibility = View.VISIBLE
         }
     }
 }
